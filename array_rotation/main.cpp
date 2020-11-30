570ec5f9e27922ba160c4fcb9007348f2b0c2e96
John Idogun <sirneij@gmail.com>
2020-11-30 11:06:51 +0100

    Digits in factoral problem

--- array_rotation/main.cpp
+++ array_rotation/main.cpp
@@ -40,6 +40,27 @@ void rotateArray(int arr[], int n, int d){
     for(size_t i = 0; i<d; i++){
         rotate(arr, n);
     }
+}
+
+//Method 3:
+
+void reverseArray(int arr[], int start, int end)
+{
+    if (start >= end) return;
+    int temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+    reverseArray(arr, start+1, end-1);
+}
+void rotate_array_new(int arr[], int n, int d){
+    if(d == 0) return;
+    reverseArray(arr, 0, d-1);
+    reverseArray(arr, d, n-1);
+    reverseArray(arr, 0, n-1);
+}
+
+
+void print_array(int arr[], int n){
     for(size_t i=0; i < n; i++){
         cout << arr[i] << "\t";
     }
@@ -50,6 +71,8 @@ int main(int argc, char const *argv[])
 {
     int arr[]={1,2,3,4,5,6,7};
     int n = sizeof(arr)/sizeof(arr[0]);
-    rotateArray(arr, n, 4);
+    rotate_array_new(arr, n, 4);
+    //reverseArray(arr, 0, n-1);
+    print_array(arr,n);
     return 0;
 }
